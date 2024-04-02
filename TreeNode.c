2fe2ec637a3416501082bab2b58589b72011598e
rockclimber147 <daylencsmith@gmail.com>
2024-04-02 09:45:33 -0700

    Add tree height and free function

--- TreeNode.c
+++ TreeNode.c
@@ -3,16 +3,40 @@
 
 typedef struct Node Node;
 struct Node {
-    char value;
+    char character;
+    int *code;
     Node *left;
     Node *right;
 };
 
+Node *createNode();
+int height(Node *root);
 void freeTree(Node *root);
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    Node *root = createNode();
+    root->left = createNode();
+    root->right = createNode();
+    root->left->left = createNode();
+    printf("Height: %d", height(root));
+    freeTree(root);
+}
+
+Node *createNode() {
+    Node *node = (Node *) malloc(sizeof(Node));
+    node->character = 127;
+    node -> code = NULL;
+    node->left = NULL;
+    node->right = NULL;
+}
+
+int height(Node *root) {
+    if (root == NULL) {
+        return -1;
+    }
+    return (height(root->left) > (height(root->right)))
+    ? height(root->left) + 1
+    : height(root->right) + 1;
 }
 
 void freeTree(Node *root) {
