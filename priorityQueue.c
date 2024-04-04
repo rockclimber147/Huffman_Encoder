a5af99d40ee5fb3aa81714a72aa15b62e17cb3a4
rockclimber147 <daylencsmith@gmail.com>
2024-04-04 13:06:16 -0700

    Add function stubs for priorityQueue.c

---
+++ priorityQueue.c
@@ -0,0 +1,24 @@
+#include "hnode.h"
+
+typedef struct QueueNode QueueNode;
+struct QueueNode {
+    Node *root;
+    int priority;
+    struct QueueNode *next;
+    struct QueueNode *prev;
+};
+
+void enqueue(QueueNode **head, QueueNode *val) {
+    // THis is responsible for inserting nodes IN ORDER
+    // First order by priority
+    // If priorities are equal, order by ASCII value of root node
+    // If ASCII values are equal, put incoming after
+}
+
+QueueNode* dequeue(QueueNode **head) {
+    // Returns the head and reassigns head pointer to new head
+}
+
+void freeQueue(QueueNode *head) {
+    // need to call freeTree on root
+}
