d095b1c7d7ebbe7afee7d3b4653ae7a5d85ae1cd
Alice Huang <alice.in.realityland@gmail.com>
2024-04-05 15:00:45 -0700

    Add logic to implement enqueue

--- priorityQueue.c
+++ priorityQueue.c
@@ -1,18 +1,50 @@
 #include "hnode.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct QueueNode QueueNode;
 struct QueueNode {
     Node *root;
     int priority;
     struct QueueNode *next;
-    struct QueueNode *prev;
 };
 
-void enqueue(QueueNode **head, QueueNode *val) {
+void insertNodeBefore(QueueNode *current, QueueNode *val) {
+    QueueNode *temp = current -> next;
+    current -> next = val;
+    val -> next = temp;
+}
+
+QueueNode enqueue(QueueNode **head, QueueNode *val) {
     // THis is responsible for inserting nodes IN ORDER
-    // First order by priority
-    // If priorities are equal, order by ASCII value of root node
-    // If ASCII values are equal, put incoming after
+    if (*head == NULL) {
+        *head = val;
+    } else {
+        if ((val -> priority < (*head) -> priority) ||
+        (val -> priority == (*head) -> priority && val -> root -> character <= (*head) -> root -> character)) {
+            // insert before head if val.priority < head.priority OR val.priority == head.priority AND val.char.ascii <= head.char.ascii
+            val -> next = (*head);
+            return *val;
+        }
+    }
+
+    QueueNode *current = *head;
+    while (current -> next != NULL) {
+        // if priorities are equal
+        if (val -> priority == current -> next -> priority) {
+            //compare ascii, insert after current
+            if (val -> root -> character <= current -> next -> root -> character) {
+                insertNodeBefore(current, val);
+                return **head;
+            }
+        } else if (val -> priority < current -> next -> priority) {
+            insertNodeBefore(current, val);
+            return **head;
+        }
+        current = current -> next;
+    }
+    current -> next = val;
+    return **head;
 }
 
 QueueNode *dequeue(QueueNode **head) {
