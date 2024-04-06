7dadd7e99039bcf2b3bc854887e0ed960cef4291
Alex Park <alexpark0930@gmail.com>
2024-04-06 13:37:52 -0700

    createTree.h add function name

--- createTree.h
+++ createTree.h
@@ -4,5 +4,9 @@
 
 #ifndef HUFFMAN_ENCODER_CREATETREE_H
 #define HUFFMAN_ENCODER_CREATETREE_H
+#include "hnode.h"
+#include "priorityQueue.h"
+
+Node* createHuffmanTree(QueueNode** head);
 
 #endif //HUFFMAN_ENCODER_CREATETREE_H
