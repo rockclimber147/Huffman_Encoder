f8dfe72d9df4c4de3efc637885e122a7288337b6
Alice Huang <alice.in.realityland@gmail.com>
2024-04-06 21:46:36 -0700

    Test

--- main.c
+++ main.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "lookups.h"
-#include "hnode.h"
-#include "priorityQueue.h"
-#include "UnitTests.h"
-#include "createTree.h"
-
-int main() {
-//    PRIORITY_QUEUE_PRINT_TEST();
-//    LOOKUP_TESTS();
-//    PRIORITY_QUEUE_TEST_ALICE();
-    CREATE_TREE_ALEX_TEST();
-    return 0;
-}
-
+////#include <stdio.h>
+////#include <stdlib.h>
+////#include "lookups.h"
+////#include "hnode.h"
+////#include "priorityQueue.h"
+////#include "UnitTests.h"
+//
+//int main() {
+////    PRIORITY_QUEUE_PRINT_TEST();
+////    LOOKUP_TESTS();
+////    PRIORITY_QUEUE_TEST_ALICE();
+//    CREATE_TREE_ALEX_TEST();
+//    return 0;
+//}
