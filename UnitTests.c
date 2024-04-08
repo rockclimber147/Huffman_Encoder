ba84b57f28b8ae06044989e6079ff1712be3b371
rockclimber147 <daylencsmith@gmail.com>
2024-04-08 14:51:36 -0700

    Clean up code

--- UnitTests.c
+++ UnitTests.c
@@ -1,7 +1,3 @@
-//
-// Created by Drumm on 2024-04-06.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "lookups.h"
@@ -111,68 +107,6 @@ int PRIORITY_QUEUE_TEST_ALICE() {
 
     return 0;
 }
-
-
-////////////////////////////////// THE FOLLOWING IS FOR TESTING ALEX'S CODE:
-
-//Node* createHuffmanTree(QueueNode** head) {
-//    while (*head != NULL && (*head)->next->next != NULL) {
-//        // Dequeue two nodes with the lowest frequencies
-//        QueueNode* node1 = dequeue(head);
-//        QueueNode* node2 = dequeue(head);
-//
-//        // Create a new internal node with node1 and node2 as children
-//        Node* newNode = createNode();
-//        newNode->left = node1->root;
-//        newNode->right = node2->root;
-//
-//        // Enqueue the new node back into the priority queue
-//        QueueNode* newQueueNode = (QueueNode*)malloc(sizeof(QueueNode));
-//        newQueueNode->root = newNode;
-//        newQueueNode->priority = node1->priority + node2->priority;
-//        newQueueNode->next = NULL;
-//        enqueue(*head, newQueueNode);
-//
-//        // Free memory for the dequeued nodes
-//        freeQueue(node1);
-//        freeQueue(node2);
-//    }
-//
-//    // The last queueNode in priority queue is the root node of Huffman Tree
-//    if (*head != NULL) {
-//        Node* huffmanRoot = (*head)->root;
-//        return huffmanRoot;
-//    }
-//
-//    return NULL; // If the queue is empty
-//}
-
-/**
- * Generates a priority queue from all nonzero characters and their frequencies in the input table
- * @return a pointer to a priority queue
- */
-//QueueNode *generatePriorityQueue(int *frequencyTable) {
-//    QueueNode *newPriorityQueue = NULL;
-//
-//    for (int i = 0; i < MAX_PRINTABLE_CHARACTERS; i++) {
-//        if (frequencyTable[i] != 0) {
-//            // Creating a new tree node for the character
-//            Node *newRoot = createNode();
-//            newRoot->character = (char) i;
-//
-//            // Creating a new queue node
-//            QueueNode *newQueueNode = createQueueNode(newRoot, frequencyTable[i]);
-//
-//            // Enqueue the new node into the priority queue
-//            if (newPriorityQueue == NULL) {
-//                newPriorityQueue = newQueueNode; // set newQueueNode as head if empty priority queue
-//            } else {
-//                newPriorityQueue = enqueue(newPriorityQueue, newQueueNode); // Enqueue in order of priority (frequency)
-//            }
-//        }
-//    }
-//    return newPriorityQueue;
-//}
 
 void TEST_freeTree() {
     Node *A = createNode();
